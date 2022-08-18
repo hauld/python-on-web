d322af952c14d6498f69012e29e4b1470f0d6b69
hau le <hauld@me.com>
2022-08-18 18:00:34 +0200

    20220818 add jest test

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    //render(<Index />)
+    //expect(something).toBeInTheDocument()
+  })
+})
